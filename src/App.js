b8d9cdeb618a0797bb75ea110d668f59970339ed
Luis <moondad2022@gmail.com>
2024-12-27 18:21:50 -0800

    states and finished api call

--- src/App.js
+++ src/App.js
@@ -1,6 +1,17 @@
 import './App.css';
 
 function App() {
+  // States: Card List, Card Grid, Selected Card
+  const [cardList, setCardList] = useState("");
+  const [cards, setCards] = useState([]);
+  const [selectedCard, setSelectedCard] = useState(null);
+
+  // Calling Scryfall API for cards
+  const fetchCards = async () => {
+    // Use regex and a map to handle different pasting formats.
+    const cardNames = cardList.split(/\r?\n/).map(name => name.trim()).filter(Boolean);
+  }
+
   return (
     <div>
     </div>
